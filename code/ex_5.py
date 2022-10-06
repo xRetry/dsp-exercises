aa2a4cc571a77887dad0029df3f8bbd1e9695eab
xRetry <marcoschwarz.at@gmail.com>
2022-10-06 18:06:11 +0200

    Add 5c

--- code/ex_5.py
+++ code/ex_5.py
@@ -12,16 +12,20 @@ plt.xlabel("k")
 plt.ylabel("$A_k$")
 plt.show()
 
-fn_true = lambda t, T: 1-2*t/T if t>0 else 1+2*t/T
-fn_fourier = lambda t, T, k: fn_ak(k) * np.cos(k * 2 * np.pi * t / T)
+fn_term = lambda t, T, k: fn_ak(k) * np.cos(k * 2 * np.pi * t / T)
 
-ts = np.linspace(-2.5, 2.5, 1000)
-y = [fn_true(t, 5) for t in ts]
+def fn_fourier(ts, k_max, T):
+    y = np.ones_like(ts) * 1/2
+    for i in range(1, k_max+1):
+        y += np.array([fn_term(t, T, i) for t in ts])
+    return y
 
-y0 = np.ones_like(ts) * 1/2
-for i in range(1, 10):
-    y0 += np.array([fn_fourier(t, 5, i) for t in ts])
+T = 5
+ts = np.linspace(-T, T, 1000)
 
-plt.plot(ts, y)
-plt.plot(ts, y0)
+plt.plot(ts, fn_fourier(ts, 1, T), label="$k_{max}=1$")
+plt.plot(ts, fn_fourier(ts, 9, T), label="$k_{max}=9$")
+plt.xticks([-T, 0, T], ["$-T_0$",0 , "$T_0$"])
+plt.ylabel("$f(x)$")
+plt.legend()
 plt.show()
